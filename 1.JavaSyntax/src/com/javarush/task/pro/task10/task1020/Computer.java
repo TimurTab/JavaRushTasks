d0c5cfb751034814572553ed905c8632dae5a120
Timur <timyrsem@gmail.com>
2024-07-06 15:58:59 +0300

    add solution java syntax task09

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Computer.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Computer.java
@@ -1,11 +1,13 @@
 package com.javarush.task.pro.task10.task1020;
 
 public class Computer {
-    public static void main(String[] args) {
-        this.monitor = new Monitor();
-        this.keyboard = new Keyboard();
-        this.systemunit = new SystemUnit();
-        this.mouse = new Mouse();
+
+
+    public Computer(){
+        Monitor monitor = new Monitor();
+        Keyboard keyboard = new Keyboard();
+        SystemUnit systemunit = new SystemUnit();
+        Mouse mouse = new Mouse();
     }
 
 }
